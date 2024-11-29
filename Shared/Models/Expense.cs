cd2be466df1db5f76628341fa8599320575fc55c
jduke788 <jduke788@gmail.com>
2024-11-29 13:38:56 +0000

    Add files via upload

---
+++ Shared/Models/Expense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerExpense.Shared.Models
+{
+    public class ExpenseModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Amount { get; set; } = 0.0M;
+        public DateTime CreatedAt { get; set; }
+    }
+}
+
